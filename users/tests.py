b7da57c7b99b7d40f62d03441e3f82be73934a96
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-09-09 22:27:40 +0200

    first commit

---
+++ users/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+# Create your tests here.
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='omar',
+            email='omar@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'omar')
+        self.assertEqual(user.email, 'omar@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='admin',
+            email='admin@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(admin_user.username, 'admin')
+        self.assertEqual(admin_user.email, 'admin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
