3d17c1601ff3f4069ff4bba08843b18ef5eb0635
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-10-04 18:51:07 +0200

    Advanced user registration

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,14 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # Third_paty
     "bootstrap4",
+    'crispy_forms',
+    'allauth',
+    'allauth.account',
 
     # local
     'users.apps.UsersConfig',  # users
     'pages.apps.PagesConfig',  # pages
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -137,5 +142,26 @@ STATICFILES_FINDERS = [
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+# django-allauth config
+
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home'
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -21,10 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # User Management
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
 
 
     # Local Apps
-    path('accounts/', include('users.urls')),
+
     path('', include('pages.urls')),
 ]
