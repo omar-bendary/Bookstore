0841747abfd618b5815bbb805ff37a140d5d2a3b
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-10-05 00:04:32 +0200

    Images upload

--- books/models.py
+++ books/models.py
@@ -13,12 +13,13 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.id)])
+        return reverse('book_detail', kwargs={'pk': str(self.pk)})
 
 
 class Review(models.Model):
